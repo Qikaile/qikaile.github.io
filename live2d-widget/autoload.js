945183e9ad1ea6a39e17043e4c58a9595d7ce2f1
sky_mirrors_the_clouds <2649019047@qq.com>
2020-03-22 18:41:47 +0800

    Site updated: 2020-03-22 18:41:44

--- live2d-widget/autoload.js
+++ live2d-widget/autoload.js
@@ -1,3 +1,3 @@
-// build time:Sun Mar 22 2020 18:36:56 GMT+0800 (GMT+08:00)
+// build time:Sun Mar 22 2020 18:41:29 GMT+0800 (GMT+08:00)
 const live2d_path="https://cdn.jsdelivr.net/gh/stevenjoezhang/live2d-widget/";$("<link>").attr({href:live2d_path+"waifu.css",rel:"stylesheet"}).appendTo("head");$.ajax({url:live2d_path+"live2d.min.js",dataType:"script",cache:true});$.ajax({url:live2d_path+"waifu-tips.js",dataType:"script",cache:true});$(window).on("load",function(){initWidget(live2d_path+"waifu-tips.json","https://live2d.fghrsh.net/api")});
 //rebuild by neat 
