981f46f4072780ac479dc05897727bd4c5cbd537
sky_mirrors_the_clouds <2649019047@qq.com>
2020-03-30 10:03:31 +0800

    Site updated: 2020-03-30 10:03:28

--- dist/music.js
+++ dist/music.js
@@ -1,3 +1,3 @@
-// build time:Sun Mar 29 2020 20:34:31 GMT+0800 (GMT+08:00)
+// build time:Mon Mar 30 2020 10:03:16 GMT+0800 (GMT+08:00)
 const ap=new APlayer({container:document.getElementById("aplayer"),fixed:true,autoplay:false,lrcType:0,audio:[{name:"无尽光芒",artist:"许巍",url:"http://www.ytmp3.cn/down/57563.mp3",cover:"https://s2.ax1x.com/2019/08/08/e7uwzn.png",lrc:"LRC/许巍-无尽光芒.lrc"},{name:"曾经的你",artist:"许巍",url:"http://www.ytmp3.cn/down/43808.mp3",cover:"https://s2.ax1x.com/2019/08/08/e7M5VK.png"},{name:"我的爱",artist:"许巍",url:"http://www.ytmp3.cn/down/59641.mp3",cover:"https://s2.ax1x.com/2019/08/08/e7M5VK.png"}]});
 //rebuild by neat 
