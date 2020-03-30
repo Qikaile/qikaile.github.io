f75e06b23f9f21b70cdc0db8fcf83a2a7a84fa6f
sky_mirrors_the_clouds <2649019047@qq.com>
2020-03-30 19:14:57 +0800

    Site updated: 2020-03-30 19:14:54

--- dist/music.js
+++ dist/music.js
@@ -1,3 +1,3 @@
-// build time:Mon Mar 30 2020 13:02:11 GMT+0800 (GMT+08:00)
+// build time:Mon Mar 30 2020 19:14:32 GMT+0800 (GMT+08:00)
 const ap=new APlayer({container:document.getElementById("aplayer"),fixed:true,autoplay:false,lrcType:0,audio:[{name:"无尽光芒",artist:"许巍",url:"http://www.ytmp3.cn/down/57563.mp3",cover:"https://s2.ax1x.com/2019/08/08/e7uwzn.png",lrc:"LRC/许巍-无尽光芒.lrc"},{name:"曾经的你",artist:"许巍",url:"http://www.ytmp3.cn/down/43808.mp3",cover:"https://s2.ax1x.com/2019/08/08/e7M5VK.png"},{name:"我的爱",artist:"许巍",url:"http://www.ytmp3.cn/down/59641.mp3",cover:"https://s2.ax1x.com/2019/08/08/e7M5VK.png"}]});
 //rebuild by neat 
--- live2d-widget/autoload.js
+++ live2d-widget/autoload.js
@@ -1,3 +1,3 @@
-// build time:Mon Mar 30 2020 13:02:11 GMT+0800 (GMT+08:00)
+// build time:Mon Mar 30 2020 19:14:33 GMT+0800 (GMT+08:00)
 const live2d_path="https://cdn.jsdelivr.net/gh/stevenjoezhang/live2d-widget/";$("<link>").attr({href:live2d_path+"waifu.css",rel:"stylesheet"}).appendTo("head");$.ajax({url:live2d_path+"live2d.min.js",dataType:"script",cache:true});$.ajax({url:live2d_path+"waifu-tips.js",dataType:"script",cache:true});$(window).on("load",function(){initWidget(live2d_path+"waifu-tips.json","https://live2d.fghrsh.net/api")});
 //rebuild by neat 
