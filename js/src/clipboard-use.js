c55931034e510e62c76854c53089e575057f3d6b
sky_mirrors_the_clouds <2649019047@qq.com>
2020-03-31 09:45:05 +0800

    Site updated: 2020-03-31 09:45:03

--- js/src/clipboard-use.js
+++ js/src/clipboard-use.js
@@ -1,3 +1,3 @@
-// build time:Mon Mar 30 2020 21:39:11 GMT+0800 (GMT+08:00)
+// build time:Tue Mar 31 2020 09:44:45 GMT+0800 (GMT+08:00)
 !function(n,t,a){var i=function(){var n="";n+='<button class="btn-copy" data-clipboard-snippet="">';n+='  <i class="fa fa-clipboard"></i><span>复制</span>';n+="</button>";$(".highlight .code pre").before(n);new ClipboardJS(".btn-copy",{target:function(n){return n.nextElementSibling}})};i()}(window,document);
 //rebuild by neat 
--- js/src/crash_cheat.js
+++ js/src/crash_cheat.js
@@ -1,3 +1,3 @@
-// build time:Mon Mar 30 2020 21:39:11 GMT+0800 (GMT+08:00)
+// build time:Tue Mar 31 2020 09:44:45 GMT+0800 (GMT+08:00)
 window.onload=function(){var e=document.title;var t;document.addEventListener("visibilitychange",function(){if(document.hidden){$('[rel="icon"]').attr("href","../../images/failure.png");$('[rel="shortcut icon"]').attr("href","../../images/failure.png");document.title="(●—●)喔哟，崩溃啦！";clearTimeout(t)}else{$('[rel="icon"]').attr("href","../../images/favicon-32x32-next.png");$('[rel="shortcut icon"]').attr("href","../../images/favicon-32x32.png");document.title="(/≧▽≦/)咦！页面又好了！";t=setTimeout(function(){document.title=e},2e3)}})};
 //rebuild by neat 
