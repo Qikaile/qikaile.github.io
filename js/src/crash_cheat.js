6e7a02a360e251fb6d0a4b7adf9a4c4592ed5acc
sky_mirrors_the_clouds <2649019047@qq.com>
2020-04-04 16:47:16 +0800

    Site updated: 2020-04-04 16:47:13

--- js/src/crash_cheat.js
+++ js/src/crash_cheat.js
@@ -1,3 +1,3 @@
-// build time:Fri Apr 03 2020 21:28:56 GMT+0800 (GMT+08:00)
+// build time:Sat Apr 04 2020 16:47:04 GMT+0800 (GMT+08:00)
 window.onload=function(){var e=document.title;var t;document.addEventListener("visibilitychange",function(){if(document.hidden){$('[rel="icon"]').attr("href","../../images/failure.png");$('[rel="shortcut icon"]').attr("href","../../images/failure.png");document.title="(●—●)喔哟，崩溃啦！";clearTimeout(t)}else{$('[rel="icon"]').attr("href","../../images/favicon-32x32-next.png");$('[rel="shortcut icon"]').attr("href","../../images/favicon-32x32.png");document.title="(/≧▽≦/)咦！页面又好了！";t=setTimeout(function(){document.title=e},2e3)}})};
 //rebuild by neat 
