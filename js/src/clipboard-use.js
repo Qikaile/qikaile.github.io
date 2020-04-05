24490e1963dae7eb20bfba7f287075e235390af8
sky_mirrors_the_clouds <2649019047@qq.com>
2020-04-05 09:05:34 +0800

    Site updated: 2020-04-05 09:05:32

--- js/src/clipboard-use.js
+++ js/src/clipboard-use.js
@@ -1,3 +1,3 @@
-// build time:Sat Apr 04 2020 16:47:04 GMT+0800 (GMT+08:00)
+// build time:Sun Apr 05 2020 09:04:59 GMT+0800 (GMT+08:00)
 !function(n,t,a){var i=function(){var n="";n+='<button class="btn-copy" data-clipboard-snippet="">';n+='  <i class="fa fa-clipboard"></i><span>复制</span>';n+="</button>";$(".highlight .code pre").before(n);new ClipboardJS(".btn-copy",{target:function(n){return n.nextElementSibling}})};i()}(window,document);
 //rebuild by neat 
