2c602f46b7d3cbe7497f102b60777c7f77a0ca43
2649019047@qq.com <2649019047@qq.com>
2020-06-12 11:34:10 +0800

    Site updated: 2020-06-12 11:34:07

--- js/src/clipboard-use.js
+++ js/src/clipboard-use.js
@@ -1,3 +1,3 @@
-// build time:Fri May 29 2020 17:33:41 GMT+0800 (GMT+08:00)
+// build time:Fri Jun 12 2020 11:33:55 GMT+0800 (GMT+08:00)
 !function(n,t,a){var i=function(){var n="";n+='<button class="btn-copy" data-clipboard-snippet="">';n+='  <i class="fa fa-clipboard"></i><span>复制</span>';n+="</button>";$(".highlight .code pre").before(n);new ClipboardJS(".btn-copy",{target:function(n){return n.nextElementSibling}})};i()}(window,document);
 //rebuild by neat 
