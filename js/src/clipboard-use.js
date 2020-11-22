1138a2784044c232801ac24c2ab8aa712978bfac
qikaile <2649019047@qq.com>
2020-11-22 16:35:19 +0800

    Site updated: 2020-11-22 16:35:16

--- js/src/clipboard-use.js
+++ js/src/clipboard-use.js
@@ -1,3 +1,3 @@
-// build time:Sun Nov 22 2020 12:33:52 GMT+0800 (GMT+08:00)
+// build time:Sun Nov 22 2020 16:32:55 GMT+0800 (GMT+08:00)
 !function(n,t,a){var i=function(){var n="";n+='<button class="btn-copy" data-clipboard-snippet="">';n+='  <i class="fa fa-clipboard"></i><span>复制</span>';n+="</button>";$(".highlight .code pre").before(n);new ClipboardJS(".btn-copy",{target:function(n){return n.nextElementSibling}})};i()}(window,document);
 //rebuild by neat 
