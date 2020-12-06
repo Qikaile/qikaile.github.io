bfd5461df5b4a549f9944bebb6612e644a475e0e
qikaile <2649019047@qq.com>
2020-12-06 13:31:10 +0800

    Site updated: 2020-12-06 13:31:07

--- js/photo.js
+++ js/photo.js
@@ -1,3 +1,3 @@
-// build time:Sat Dec 05 2020 17:24:35 GMT+0800 (GMT+08:00)
+// build time:Sun Dec 06 2020 13:30:54 GMT+0800 (GMT+08:00)
 photo={page:1,offset:100,init:function(){var t=this;$.getJSON("/photos/photoslist.json",function(i){t.render(t.page,i)})},render:function(t,i){var r=(t-1)*this.offset;var e=t*this.offset;if(r>=i.length)return;var a,n,o,s,d,c,f="";for(var g=r;g<e&&g<i.length;g++){n=i[g].url;o=i[g].title;f+='<div class="card" style="width:250px">'+'<div class="ImageInCard" style="height:'+250+'px">'+'<a data-fancybox="gallery" href="'+n+'?raw=true" data-caption="'+o+'">'+'<img class="CpicView" srcset="'+n+'-toSmall?raw=true" src="'+n+'?raw=true"/>'+"</a>"+"</div>"+"</div>"}$(".ImageGrid").append(f);this.minigrid()},minigrid:function(){var t=new Minigrid({container:".ImageGrid",item:".card",gutter:12});t.mount();$(window).resize(function(){t.mount()})}};photo.init();
 //rebuild by neat 
--- js/src/crash_cheat.js
+++ js/src/crash_cheat.js
@@ -1,3 +1,3 @@
-// build time:Sat Dec 05 2020 17:24:37 GMT+0800 (GMT+08:00)
+// build time:Sun Dec 06 2020 13:30:55 GMT+0800 (GMT+08:00)
 window.onload=function(){var e=document.title;var t;document.addEventListener("visibilitychange",function(){if(document.hidden){$('[rel="icon"]').attr("href","../../images/failure.png");$('[rel="shortcut icon"]').attr("href","../../images/failure.png");document.title="(●—●)喔哟，崩溃啦！";clearTimeout(t)}else{$('[rel="icon"]').attr("href","../../images/favicon-32x32-next.png");$('[rel="shortcut icon"]').attr("href","../../images/favicon-32x32.png");document.title="(/≧▽≦/)咦！页面又好了！";t=setTimeout(function(){document.title=e},2e3)}})};
 //rebuild by neat 
