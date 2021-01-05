c3c879f981536d45d8f8eedd4e8dee37a165da27
qikaile <2649019047@qq.com>
2021-01-05 16:18:54 +0800

    Site updated: 2021-01-05 16:18:51

--- js/src/clipboard-use.js
+++ js/src/clipboard-use.js
@@ -1,3 +1,3 @@
-// build time:Sat Jan 02 2021 12:37:27 GMT+0800 (GMT+08:00)
+// build time:Tue Jan 05 2021 16:18:40 GMT+0800 (GMT+08:00)
 !function(n,t,a){var i=function(){var n="";n+='<button class="btn-copy" data-clipboard-snippet="">';n+='  <i class="fa fa-clipboard"></i><span>复制</span>';n+="</button>";$(".highlight .code pre").before(n);new ClipboardJS(".btn-copy",{target:function(n){return n.nextElementSibling}})};i()}(window,document);
 //rebuild by neat 
