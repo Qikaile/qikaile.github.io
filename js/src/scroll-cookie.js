8fcf5a33c4aac08d80416b752db9ccd8f407143e
qikaile <2649019047@qq.com>
2021-03-01 15:04:36 +0000

    Site updated: 2021-03-01 15:04:35

--- js/src/scroll-cookie.js
+++ js/src/scroll-cookie.js
@@ -1,3 +1,3 @@
-// build time:Mon Mar 01 2021 14:56:23 GMT+0000 (Coordinated Universal Time)
+// build time:Mon Mar 01 2021 15:04:28 GMT+0000 (Coordinated Universal Time)
 $(document).ready(function(){var o=window.location.href.replace(window.location.origin,"");var i;$(window).on("scroll",function(){clearTimeout(i);i=setTimeout(function(){Cookies.set("scroll-cookie",$(window).scrollTop()+"|"+o,{expires:365,path:""})},250)});if(Cookies.get("scroll-cookie")!==undefined){var e=Cookies.get("scroll-cookie").split("|");if(e[1]==o){$(window).scrollTop(e[0])}}});
 //rebuild by neat 
