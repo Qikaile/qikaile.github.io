e868d920e328b817a555bce706ae666159a43fd2
qikaile <2649019047@qq.com>
2021-03-03 02:17:27 +0000

    Site updated: 2021-03-03 02:17:26

--- js/src/crash_cheat.js
+++ js/src/crash_cheat.js
@@ -1,3 +1,3 @@
-// build time:Wed Mar 03 2021 02:13:12 GMT+0000 (Coordinated Universal Time)
+// build time:Wed Mar 03 2021 02:17:19 GMT+0000 (Coordinated Universal Time)
 window.onload=function(){var e=document.title;var t;document.addEventListener("visibilitychange",function(){if(document.hidden){$('[rel="icon"]').attr("href","../../images/failure.png");$('[rel="shortcut icon"]').attr("href","../../images/failure.png");document.title="(●—●)喔哟，崩溃啦！";clearTimeout(t)}else{$('[rel="icon"]').attr("href","../../images/favicon-32x32-next.png");$('[rel="shortcut icon"]').attr("href","../../images/favicon-32x32.png");document.title="(/≧▽≦/)咦！页面又好了！";t=setTimeout(function(){document.title=e},2e3)}})};
 //rebuild by neat 
