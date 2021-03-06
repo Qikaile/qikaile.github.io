9868160ac803f360b49f20dfb38575a2708911a5
qikaile <2649019047@qq.com>
2021-03-06 03:09:22 +0000

    Site updated: 2021-03-06 03:09:21

--- js/src/scroll-cookie.js
+++ js/src/scroll-cookie.js
@@ -1,3 +1,3 @@
-// build time:Fri Mar 05 2021 10:34:02 GMT+0000 (Coordinated Universal Time)
+// build time:Sat Mar 06 2021 03:09:13 GMT+0000 (Coordinated Universal Time)
 $(document).ready(function(){var o=window.location.href.replace(window.location.origin,"");var i;$(window).on("scroll",function(){clearTimeout(i);i=setTimeout(function(){Cookies.set("scroll-cookie",$(window).scrollTop()+"|"+o,{expires:365,path:""})},250)});if(Cookies.get("scroll-cookie")!==undefined){var e=Cookies.get("scroll-cookie").split("|");if(e[1]==o){$(window).scrollTop(e[0])}}});
 //rebuild by neat 
