ca5f11449e1055fe5a1ce2122eda7182b3f808c7
qikaile <2649019047@qq.com>
2021-03-19 01:36:55 +0000

    Site updated: 2021-03-19 01:36:54

--- js/src/clipboard-use.js
+++ js/src/clipboard-use.js
@@ -1,3 +1,3 @@
-// build time:Sun Mar 14 2021 11:43:19 GMT+0000 (Coordinated Universal Time)
+// build time:Fri Mar 19 2021 01:36:47 GMT+0000 (Coordinated Universal Time)
 !function(n,t,a){var i=function(){var n="";n+='<button class="btn-copy" data-clipboard-snippet="">';n+='  <i class="fa fa-clipboard"></i><span>复制</span>';n+="</button>";$(".highlight .code pre").before(n);new ClipboardJS(".btn-copy",{target:function(n){return n.nextElementSibling}})};i()}(window,document);
 //rebuild by neat 
--- js/src/scroll-cookie.js
+++ js/src/scroll-cookie.js
@@ -1,3 +1,3 @@
-// build time:Sun Mar 14 2021 11:43:19 GMT+0000 (Coordinated Universal Time)
+// build time:Fri Mar 19 2021 01:36:47 GMT+0000 (Coordinated Universal Time)
 $(document).ready(function(){var o=window.location.href.replace(window.location.origin,"");var i;$(window).on("scroll",function(){clearTimeout(i);i=setTimeout(function(){Cookies.set("scroll-cookie",$(window).scrollTop()+"|"+o,{expires:365,path:""})},250)});if(Cookies.get("scroll-cookie")!==undefined){var e=Cookies.get("scroll-cookie").split("|");if(e[1]==o){$(window).scrollTop(e[0])}}});
 //rebuild by neat 
