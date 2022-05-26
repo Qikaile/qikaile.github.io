b8f92a9b3903c71d89dccf1dd008fe461389b458
qikaile <2649019047@qq.com>
2022-05-26 11:05:24 +0000

    Site updated: 2022-05-26 11:05:23

--- js/photo.js
+++ js/photo.js
@@ -1,3 +1,3 @@
-// build time:Mon May 23 2022 13:57:30 GMT+0000 (Coordinated Universal Time)
+// build time:Thu May 26 2022 11:05:14 GMT+0000 (Coordinated Universal Time)
 photo={page:1,offset:100,init:function(){var t=this;$.getJSON("/photos/photoslist.json",function(i){t.render(t.page,i)})},render:function(t,i){var r=(t-1)*this.offset;var e=t*this.offset;if(r>=i.length)return;var a,n,o,s,d,c,f="";for(var g=r;g<e&&g<i.length;g++){n=i[g].url;o=i[g].title;f+='<div class="card" style="width:250px">'+'<div class="ImageInCard" style="height:'+250+'px">'+'<a data-fancybox="gallery" href="'+n+'?raw=true" data-caption="'+o+'">'+'<img class="CpicView" srcset="'+n+'-toSmall?raw=true" src="'+n+'?raw=true"/>'+"</a>"+"</div>"+"</div>"}$(".ImageGrid").append(f);this.minigrid()},minigrid:function(){var t=new Minigrid({container:".ImageGrid",item:".card",gutter:12});t.mount();$(window).resize(function(){t.mount()})}};photo.init();
 //rebuild by neat 
